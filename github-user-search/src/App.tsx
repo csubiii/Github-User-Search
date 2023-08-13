ce135c5c43a9179b3f53b6f287fff508246b348e
csubiii <csubicsabi05@gmail.com>
2023-08-13 03:52:04 +0200

    😲

--- github-user-search/src/App.tsx
+++ github-user-search/src/App.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import Profile from './Profile';
+
+type UserData = {
+  login: string,
+  avatar_url: string,
+  location: string,
+  bio: string,
+  followers: number,
+  following: number,
+}
 
 const App = () => {
+
+  const [data, setData] = useState<UserData | null>(null);
+  const name = useRef<HTMLInputElement | null>(null);
+  const [username, setUsername] = useState<string>('');
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (name.current) {
+      setUsername(name.current.value);
+    }
+  };
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (username) {
+        try {
+          const response = await fetch(`https://api.github.com/users/${username}`);
+          const resData = await response.json();
+          setData(resData);
+          console.log(data);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    };
+    
+    fetchUser();
+  }, [username]);
+  
+
   return (
-    <div className='text-3xl font-bold underline'>App</div>
-  )
+    <div className="flex items-center justify-center w-[310px] mx-auto mt-5">
+      <div className="flex flex-col items-center space-y-4">
+        <div className="flex gap-[10px] w-[310px]">
+          <input className='w-full outline outline-1 outline-[#343a41] p-1 rounded-md bg-transparent text-[#e9f0f6]' type='text' placeholder='Github username' ref={name} />
+          <button className='outline outline-1 outline-[#343a41] p-1 rounded-md' onClick={handleClick}>🔎</button>
+        </div>
+        {data && <Profile data={data} />}
+      </div>
+    </div>
+  );
 }
+  
 
 export default App
---
+++ github-user-search/src/Profile.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Profile = ({ data }) => {
+  return (
+    <div className='outline outline-1 outline-[#343a41] p-3 rounded-md w-[310px]'>
+      <div className="flex gap-2">
+        <div className="flex items-center justify-center">
+          <img
+            src={data.avatar_url}
+            alt={`${data.login}'s avatar`}
+            className='rounded-full object-cover'
+            style={{ maxWidth: '70px', maxHeight: '70px' }}
+          />
+        </div>
+        <div className="text-sm">
+          <h2 className='text-[#707782] text-lg'>{data.login}</h2>
+          <p className='text-[#e9f0f6]'>{data.location && (`📍 ${data.location}`)}</p>
+          <div className="flex gap-1 text-[#e9f0f6]">
+            <p>{data.followers} <span className='text-[#707782]'>follower</span></p>
+            <p className='flex items-center justify-center w-1 h-4 text-[#e9f0f6]'>.</p>
+            <p>{data.following} <span className='text-[#707782]'>following</span></p>
+          </div>
+          <p className='text-[#e9f0f6]'>📔 <span className='text-[#707782]'>Repos</span> {data.public_repos}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
